df0fe613c6cde92d1da14598eb5cd5d03a74652b
Pawel Niznik <pawelniznik@data.futuresimple.local>
2012-07-12 13:55:11 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackkrkHaService::Application.config.secret_token = 'b32bfbfeb79cfe5558ab40896f377fe885f0adcccc739f94a46db830539f51c9e09fe6eea2d56d6c98e267d0ff4ee0cfdeff26f2d0c4e51a4dade42aba05857a'
